4412f32499abaf271d6abf76c9af94afea66b8b0
Sain Choi <chyh1900@gmail.com>
2020-03-21 04:38:28 +0800

    Revert "이벤트 섹션 구현 완료 (#18)" (#22)
    This reverts commit 6ca71b4326f0e5458d924434ec22d433cf3fa06a.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React from 'react';
-import './styles/common.scss';
-import Wrapper from './components/Wrapper';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Main from './pages/Main';
-import { Route } from 'react-router-dom';
-
-if (!localStorage.getItem('lang')) {
-    localStorage.setItem('lang', 'ko');
-    window.lang = 'ko';
-}
-window.lang = localStorage.getItem('lang');
+import React from "react";
 
 function App() {
-    return (
-        <>
-            <Nav />
-            <Wrapper>
-                <Route path="/" component={Main} exact />
-            </Wrapper>
-            <Footer />
-        </>
-    );
+  return <div>Eduity</div>;
 }
 
 export default App;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import Section1 from '../components/Section1';
-import Section2 from '../components/Section2';
-import Section3 from '../components/Section3';
-// import SectionEvents from '../components/SectionEvents';
-import SectionContactUs from '../components/SectionContactUs';
-import SectionLocation from '../components/SectionLocation';
 
 function Main() {
-    return (
-        <>
-            <Section1 />
-            <Section2 />
-            <Section3 />
-            {/* <SectionEvents /> */}
-            <SectionContactUs />
-            <SectionLocation />
-        </>
-    );
+    return <div>Eduity</div>;
 }
 
 export default Main;
