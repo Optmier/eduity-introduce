f98f2d1f16557f740047492a69c85e2249ffc0df
JunYoung Choi <jun094@naver.com>
2020-03-25 23:47:14 +0900

    라우터

---
+++ src/components/contents.js
@@ -0,0 +1,51 @@
+<div className="contents">
+<div className="title">
+    <p>{product.prods.item_1.title[window.lang]}</p>
+</div>
+<div className="strong">
+    <p>{product.prods.item_1.strong[window.lang]}</p>
+</div>
+<div className="content">
+    <p className="m">{product.prods.item_1.p_1[window.lang]}</p>
+    <p className="s">{product.prods.item_1.s_1[window.lang]}</p>
+    <p className="m">{product.prods.item_1.p_2[window.lang]}</p>
+    <p className="s">{product.prods.item_1.s_2[window.lang]}</p>
+    <p className="m">{product.prods.item_1.p_3[window.lang]}</p>
+    <p className="s">{product.prods.item_1.s_3[window.lang]}</p>
+</div>
+</div>
+<div className="contents">
+<div className="title">
+    <p>{product.prods.item_2.title[window.lang]}</p>
+</div>
+<div className="strong">
+    <p>{product.prods.item_2.strong[window.lang]}</p>
+</div>
+<div className="content">
+    <p>{product.prods.item_2.p_1[window.lang]}</p>
+    <p>{product.prods.item_2.p_2[window.lang]}</p>
+</div>
+</div>
+<div className="contents">
+<div className="title">
+    <p>{product.prods.item_3.title[window.lang]}</p>
+</div>
+<div className="strong">
+    <p>{product.prods.item_3.strong[window.lang]}</p>
+</div>
+<div className="content">
+    <p>{product.prods.item_3.p_1[window.lang]}</p>
+</div>
+</div>
+<div className="contents">
+<div className="title">
+    <p>{product.prods.item_4.title[window.lang]}</p>
+</div>
+<div className="strong">
+    <p>{product.prods.item_4.strong[window.lang]}</p>
+</div>
+<div className="content">
+    <p>{product.prods.item_4.p_1[window.lang]}</p>
+    <p>{product.prods.item_4.p_2[window.lang]}</p>
+</div>
+</div>
